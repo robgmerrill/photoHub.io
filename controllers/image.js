db16cbfcd815489f7ae84aaed634c89ab8f88d7d
Rob Merrill <robgmerrill@gmail.com>
2016-03-19 15:06:14 -0700

    view models and controllers synced

--- controllers/image.js
+++ controllers/image.js
@@ -1,13 +1,75 @@
+var sidebar = require('../helpers/sidebar');
+var fs = require('fs');
+var path = require('path');
+
 module.exports = {
   index: function(req, res) {
-    res.send('The image: index controller ' + req.params.image_id);
+    var viewModel = {
+    image: {
+        uniqueId:       1,
+        title:          'Sample Image 1',
+        description:    'This is a sample.',
+        filename:       'sample1.jpg',
+        views:          0,
+        likes:          0,
+        // timestamp:      Date.nowDate.now()
+    },
+    comments: [
+        {
+            image_id:   1,
+            email:      'test@testing.com',
+            name:       'Test Tester',
+            gravatar:   'http://lorempixel.com/75/75/animals/1',
+            comment:    'This is a test comment...',
+            timestamp:  Date.now()
+        },{
+            image_id:   1,
+            email:      'test@testing.com',
+            name:       'Test Tester',
+            gravatar:   'http://lorempixel.com/75/75/animals/2',
+            comment:    'Another followup comment!',
+            timestamp:  Date.now()
+        }
+    ]
+};
+
+sidebar(viewModel,function(viewModel){
+res.render('image', viewModel);
+});
   },
   create: function(req, res) {
-    res.send('The image:create POST controller');
-  },
+        var saveImage = function() {
+            var possible = 'abcdefghijklmnopqrstuvwxyz0123456789',
+                imgUrl = '';
+
+            for(var i=0; i < 6; i+=1) {
+                imgUrl += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+
+            var tempPath = req.files.file.path,
+                ext = path.extname(req.files.file.name).toLowerCase(),
+                targetPath = path.resolve('./public/upload/' + imgUrl + ext);
+
+            if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+                fs.rename(tempPath, targetPath, function(err) {
+                    if (err) throw err;
+
+                    res.redirect('/images/' + imgUrl);
+                });
+            } else {
+                fs.unlink(tempPath, function () {
+                    if (err) throw err;
+
+                    res.json(500, {error: 'Only image files are allowed.'});
+                });
+            }
+        };
+
+        saveImage();
+    },
   like: function(req, res) {
-    res.send('The image:like POST controller');
-  },
+    res.json({likes: 1});
+},
   comment: function(req, res) {
     res.send('The image: comment POST controller');
   }
--- server/configure.js
+++ server/configure.js
@@ -7,26 +7,36 @@ var cookieParser = require('cookie-parser');
 var morgan = require('morgan');
 var methodOverride = require('method-override');
 var errorHandler = require('errorhandler');
+var moment = require('moment');
+var multer = require('multer');
 
 module.exports = function(app) {
-app.use(morgan('dev'));
-   app.use(bodyParser({
-       uploadDir:path.join(__dirname, 'public/upload/temp')
-}));
+  app.engine('handlebars', exphbs.create({
+        defaultLayout: 'main',
+        layoutsDir: app.get('views') + '/layouts',
+        partialsDir: [app.get('views') + '/partials'],
+        helpers: {
+            timeago: function(timestamp) {
+                console.log(timestamp);
+                return moment(timestamp).startOf('minute').fromNow();
+            }
+        }
+    }).engine);
+    app.set('view engine', 'handlebars');
+
+  app.use(morgan('dev'));
+  app.use(multer({ dest: path.join(__dirname, 'public/upload/temp')}));
+
   app.use(methodOverride());
   app.use(cookieParser('some-secret-value-here'));
   routes(app);
+  
   app.use('/public/', express.static(path.join(__dirname, '../public')));
 
+  if ('development' === app.get('env')) {
+     app.use(errorHandler());
+  }
 
-if('development' === app.get('env')) {
-  app.use(errorHandler());
-}
-  app.engine('handlebars', exphbs.create({
-    defaultLayout: 'main', 
-    layoutsDir: app.get('views') + '/layouts',
-    partialsDir: [app.get('views') + '/partials']
-  }).engine);
-  app.set('view engine', 'handlebars');
-  return app;
+    return app;
 };
+
