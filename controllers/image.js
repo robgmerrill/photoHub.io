2fe881d4f86f994839dc32269cf0c97bc15b78e6
Rob Merrill <robgmerrill@gmail.com>
2016-03-21 07:54:09 -0700

    likes and comments are database

--- controllers/image.js
+++ controllers/image.js
@@ -79,9 +79,59 @@ module.exports = {
         saveImage();
     },
   like: function(req, res) {
-    res.json({likes: 1});
-},
-  comment: function(req, res) {
-    res.send('The image: comment POST controller');
-  }
+        Models.Image.findOne({ filename: { $regex: req.params.image_id } },
+            function(err, image) {
+                if (!err && image) {
+                    image.likes = image.likes + 1;
+                    image.save(function(err) {
+                        if (err) {
+                            res.json(err);
+                        } else {
+                            res.json({ likes: image.likes });
+                        }
+                    });
+                }
+            });
+    },
+    comment: function(req, res) {
+        Models.Image.findOne({ filename: { $regex: req.params.image_id } },
+            function(err, image) {
+                if (!err && image) {
+                    var newComment = new Models.Comment(req.body);
+                    console.log("DOOO")
+                    console.log(req.body)
+                    newComment.gravatar = md5(newComment.email);
+                    newComment.image_id = image._id;
+                    newComment.save(function(err, comment) {
+                        if (err) { throw err; }
+
+                        res.redirect('/images/' + image.uniqueId + '#' + comment._id);
+                    });
+                } else {
+                    res.redirect('/');
+                }
+            });
+    },
+    remove: function(req, res) {
+        Models.Image.findOne({ filename: { $regex: req.params.image_id } },
+            function(err, image) {
+                if (err) { throw err; }
+
+                fs.unlink(path.resolve('./public/upload/' + image.filename),
+                    function(err) {
+                        if (err) { throw err; }
+
+                        Models.Comment.remove({ image_id: image._id},
+                            function(err) {
+                                image.remove(function(err) {
+                                    if (!err) {
+                                        res.json(true);
+                                    } else {
+                                        res.json(false);
+                                    }
+                                });
+                        });
+                });
+            });
+    }
 };
